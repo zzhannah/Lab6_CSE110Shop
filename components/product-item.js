5a3883645344eae8c169ed7b86fee31a1689f7d2
crasque <crasque@pint.com>
2021-02-09 15:03:15 -0800

    First commit

---
+++ components/product-item.js
@@ -0,0 +1,7 @@
+// product-item.js
+
+class ProductItem extends HTMLElement {
+  // TODO
+}
+
+customElements.define('product-item', ProductItem);
---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+// Script.js
+
+window.addEventListener('DOMContentLoaded', () => {
+  // TODO
+});
